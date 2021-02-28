9e14b5c5b6ffe6445c30f9422f3c1e8e7119e6b4
Grace Garcia <gracegarcia0803@gmail.com>
2021-02-28 19:47:27 +0800

    Added switch theme functionality

---
+++ script.js
@@ -0,0 +1,46 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+const img = document.querySelector('img');
+
+// Dark or Light Images
+function imageMode(color) {
+  img.src = `./img/bike_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imageMode('dark');
+}
+
+// Light Mode Styles
+function lightMode() {
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imageMode('light');
+}
+
+// Set theme
+toggleSwitch.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    lightMode();
+  }
+});
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
